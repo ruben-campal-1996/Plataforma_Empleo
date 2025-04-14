af512c8c0935a562b2768a0edbc96c6207df903e
ruben.campal <putosnombre@gmail.com>
2025-04-14 14:54:26 +0200

    Implementación vista básica para retocar detalles usuario

--- Plataforma/Usuarios/urls.py
+++ Plataforma/Usuarios/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
+    path('perfil/', views.user_details, name='user_details'),
     path('gestion_usuarios/', views.gestion_usuarios, name='gestion_usuarios'),
 ]
